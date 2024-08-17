60a3bfcca38b4e462f86098288720e19b4341710
roblesdotdev <httpsnip@gmail.com>
2024-08-17 18:39:22 -0300

    config ts alias

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
 import eslintPluginAstro from 'eslint-plugin-astro'
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      'no-duplicate-imports': 'error',
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 ]
