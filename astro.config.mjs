b1fd62b64a333ed11fce98f3bf54cf9639fced53
roblesdotdev <httpsnip@gmail.com>
2024-08-17 21:36:53 -0300

    setup sitemap & seo meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://quick-astro.pages.dev',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 })
