4ad47a67298c9c51faead3778004fefa77a14228
roblesdotdev <httpsnip@gmail.com>
2024-08-18 17:25:51 -0300

    home minimal styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from 'tailwindcss'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--sans-font)', 'sans-serif'],
+        display: ['var(--display-font)', 'serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
